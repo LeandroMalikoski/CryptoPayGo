951846f7b48c5ac665a88795e53066549afe70bb
Leandro Malikoski <leandroborgesmalikoski@gmail.com>
2025-03-12 10:45:31 -0300

    chore: Start Spring Boot Project

---
+++ src/test/java/com/cryptopaygo/CryptoPayGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptopaygo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPayGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
