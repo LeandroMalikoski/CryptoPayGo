a7a7fc29c944af847851e04b456fb18fd2f6e21d
Leandro Malikoski <leandroborgesmalikoski@gmail.com>
2025-03-12 11:08:17 -0300

    feat: create user entity and access rollers

---
+++ src/main/java/com/cryptopaygo/config/enums/Role.java
@@ -0,0 +1,25 @@
+package com.cryptopaygo.config.enums;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+public enum Role {
+    ADMIN("ADMIN"),
+    USER("USER");
+
+    private final String name;
+
+    Role(String name) {
+        this.name = name;
+    }
+
+    public static Role fromString(String name) {
+        return Arrays.stream(Role.values())
+                .filter(role -> role.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No constant with name %s found in Role".formatted(name)));
+    }
+
+}
