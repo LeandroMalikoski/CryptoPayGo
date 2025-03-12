502e895aa99f6da1bcf3f7acf117272d14df6a52
Leandro Malikoski <leandroborgesmalikoski@gmail.com>
2025-03-12 14:38:13 -0300

    chore: move CorsConfiguration package

--- src/main/java/com/cryptopaygo/config/CorsConfiguration.java
+++ src/main/java/com/cryptopaygo/config/CorsConfiguration.java
@@ -1,4 +1,4 @@
-package com.cryptopaygo;
+package com.cryptopaygo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
