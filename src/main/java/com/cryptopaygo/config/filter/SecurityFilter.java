777124134d8d812e0cee2a82913e7d718f8ce031
Leandro Malikoski <leandroborgesmalikoski@gmail.com>
2025-03-16 21:00:10 -0300

    feat: add sendErrorResponse method for receive json response

--- src/main/java/com/cryptopaygo/config/filter/SecurityFilter.java
+++ src/main/java/com/cryptopaygo/config/filter/SecurityFilter.java
@@ -1,16 +1,22 @@
 package com.cryptopaygo.config.filter;
 
+import com.cryptopaygo.config.exception.TokenInvalidException;
 import com.cryptopaygo.config.repository.UserRepository;
 import com.cryptopaygo.config.service.TokenService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
@@ -25,22 +31,40 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            var tokenJwt = recoverToken(request);
 
-        var tokenJwt = recoverToken(request);
+            if (tokenJwt != null) {
+                var userName = tokenService.validateToken(tokenJwt);
+                var user = userRepository.findByEmail(userName);
+                if (user.isEmpty()) {
+                    throw new RuntimeException("User not found");
+                }
 
-        if (tokenJwt != null) {
-            var userName = tokenService.validateToken(tokenJwt);
-            var user = userRepository.findByEmail(userName);
-            if (user.isEmpty()) {
-                throw new RuntimeException("User not found");
+                var authentication = new UsernamePasswordAuthenticationToken(user.get().getEmail(), null, user.get().getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
 
-            var authentication = new UsernamePasswordAuthenticationToken(user.get().getEmail(), null, user.get().getAuthorities());
+            filterChain.doFilter(request, response);
 
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (TokenInvalidException ex) {
+            sendErrorResponse(response, ex.getMessage(), HttpStatus.UNAUTHORIZED);
+        } catch (RuntimeException ex) {
+            sendErrorResponse(response, "Authentication error", HttpStatus.UNAUTHORIZED);
         }
+    }
 
-        filterChain.doFilter(request, response);
+    private void sendErrorResponse(HttpServletResponse response, String message, HttpStatus status) throws IOException {
+        response.setStatus(status.value());
+        response.setContentType("application/json");
+
+        Map<String, Object> errorDetails = new HashMap<>();
+        errorDetails.put("message", message);
+        errorDetails.put("status", false);
+
+        ObjectMapper mapper = new ObjectMapper();
+        response.getWriter().write(mapper.writeValueAsString(errorDetails));
     }
 
     public String recoverToken(HttpServletRequest request) {
