9c0c51a3dfd85f20d45fd56edc82697f43d54e99
Leandro Malikoski <leandroborgesmalikoski@gmail.com>
2025-03-16 21:23:42 -0300

    refactor: update references by GeneralResponseDTO

--- src/main/java/com/cryptopaygo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cryptopaygo/exception/GlobalExceptionHandler.java
@@ -3,7 +3,7 @@ package com.cryptopaygo.exception;
 import com.cryptopaygo.config.exception.RegisterInvalidException;
 import com.cryptopaygo.config.exception.TokenInvalidException;
 import com.cryptopaygo.config.exception.UserNotFoundException;
-import com.cryptopaygo.dto.ErrorResponseDTO;
+import com.cryptopaygo.dto.GeneralResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -15,49 +15,49 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RegisterInvalidException.class)
-    public ResponseEntity<ErrorResponseDTO> handleRegisterInvalidException(RegisterInvalidException e) {
+    public ResponseEntity<GeneralResponseDTO> handleRegisterInvalidException(RegisterInvalidException e) {
         // Retorna uma resposta com status 409 (CONFLICT) e a mensagem de erro
         return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(new ErrorResponseDTO(e.getMessage(), false));
+                .body(new GeneralResponseDTO(e.getMessage(), false));
     }
 
     // Tratar exceção quando login falhar
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ErrorResponseDTO> handleBadCredentials() {
+    public ResponseEntity<GeneralResponseDTO> handleBadCredentials() {
         // Retorna uma resposta com status 401 (UNAUTHORIZED) e a mensagem de erro
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(new ErrorResponseDTO("Non-existent user or invalid password", false));
+                .body(new GeneralResponseDTO("Non-existent user or invalid password", false));
     }
 
     // Tratar exceção quando o Usuário não for encontrado
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ErrorResponseDTO> handleUserNotFound(UserNotFoundException e) {
+    public ResponseEntity<GeneralResponseDTO> handleUserNotFound(UserNotFoundException e) {
         // Retorna uma resposta com status 404 (NOT FOUND) e a mensagem de erro
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ErrorResponseDTO(e.getMessage(), false));
+                .body(new GeneralResponseDTO(e.getMessage(), false));
     }
 
     // Tratar exceção quando token for inválido ou expirado
     @ExceptionHandler(TokenInvalidException.class)
-    public ResponseEntity<ErrorResponseDTO> handleTokenInvalidException(TokenInvalidException e) {
+    public ResponseEntity<GeneralResponseDTO> handleTokenInvalidException(TokenInvalidException e) {
         // Retorna uma resposta com status 401 (UNAUTHORIZED) e a mensagem de erro
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(new ErrorResponseDTO(e.getMessage(), false));
+                .body(new GeneralResponseDTO(e.getMessage(), false));
     }
 
     // Tratar exceção quando o ID fornecido não pode ser convertido
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<ErrorResponseDTO> handleInvalidId() {
+    public ResponseEntity<GeneralResponseDTO> handleInvalidId() {
         // Retorna uma resposta com status 400 (BAD REQUEST) e a mensagem de erro
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorResponseDTO("Invalid ID format", false));
+                .body(new GeneralResponseDTO("Invalid ID format", false));
     }
 
     // Exceção para erros genéricos
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponseDTO> handleGenericException() {
+    public ResponseEntity<GeneralResponseDTO> handleGenericException() {
         // Retorna uma resposta com status 500 (INTERNAL SERVER ERROR) e a mensagem de erro
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponseDTO("Unexpected error occurred", false));
+                .body(new GeneralResponseDTO("Unexpected error occurred", false));
     }
 }
